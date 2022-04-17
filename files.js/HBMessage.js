d75d07180236b1c6c53e39e0edfdfe407af9ebeb
Luiz <luizfranzon05@hotmail.com>
2022-04-17 14:34:15 -0300

    Happy Birthday Generic Message Generator

---
+++ files.js/HBMessage.js
@@ -0,0 +1,21 @@
+// Usage: node happyBirthday.js Destinatario Remetende
+
+const personName = process.argv[2]
+const forWho = process.argv[3]
+
+console.log(' ')
+console.log(`${personName}, hoje é seu dia! Parabéns!
+Primeiramente quero te desejar um feliz aniversário, ${personName}.
+Que você faça muitos e muitos anos, e que sejam todos felizes e abençoados e principalmente cheios de paz e harmonia, pois uma pessoa como você ${personName}, merece.
+Quero que você saiba ${personName} que fico muito feliz em tê-la ao meu lado todos os dias, agradeço por você ser essa amiga tão maravilhosa que sempre está do meu lado em qualquer momento, seja bom ou ruim, até aqueles para dar uma “bronquinha” :)
+É muito bom quando a gente encontra uma amiga especial ${personName}, aquela que faz nossos dias mais bonitos (como você ${personName}), mais alegres e especiais (também como você ${personName}), aquela que faz você acreditar que o mundo tem algo bom te esperando, que ri e fica triste junto com você!
+Uma pessoa tão boa que te recebe de braços abertos! e essa pessoa é você ${personName}!!!!
+Aquela que te faz acreditar que sempre há uma chave, para uma porta destrancada. Que te faz seguir em frente. Você é tudo isso pra mim.
+Quero que saiba ${personName} que sempre estarei com você nas horas difíceis e confusas, que vou te pôr pra cima, quando você estiver pra baixo, que vou clarear seu dia quando ele estiver escuro assim como você faz comigo, ${personName}.
+Saiba que você significa muito pra mim, muito mesmo! Ter você como AMIGA! não tem preço, ${personName}.
+Que Deus abençoe você ${personName} e ilumine seu caminho assim como você faz com o meu, ${personName}, e que a cada dia que passe, que ele te guie e que você sempre saiba qual o caminho certo a seguir!
+Serei para sempre sua amiga.
+Te amo, ${personName}!
+
+Com muito amor. ${forWho}`)
+console.log(' ')
